9e05584ad151f7c21b7eca48e3eb84e4edd49119
David Caro <david.caro.dec@hotmail.com>
2022-09-24 13:24:58 -0300

    created tasks route controllers: delete and update

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,26 +1,95 @@
 const Task = require("../models/task");
 
-const postTask = async (req, res) => {
+const catchError = (res, error) => {
+  return res.status(400).json({
+    error,
+    message: error.message
+  })
+}
+
+const getTasks = async(req, res) => {
+  const { name } = req.params;
+  try {
+    if (name) {
+      const task = await Task.findOne({ name });
+      return res.status(200).json({
+        message: '',
+        task
+      })
+    }
+  
+    const tasks = await Task.find({});
+    return res.status(200).json({
+      message: '',
+      tasks
+    })
+  } catch (error) {
+    catchError(res, error);
+  }
+}
+
+const postTask = async(req, res) => {
   const { name, description, list } = req.body;
-  if (name.length < 2)  return res.json({ message: "Please insert a name to the task" });
+
+  if (name.length < 2) {
+    return res.status(400).json({ error: true, message: "Please insert a name to the task" });
+  }
 
   const data = { name, description, list };
   try {
-    const newTask = new Task({
+    const task = new Task(data);
+    await task.save();
+
+    return res.status(200).json({
       message: "Task created successfully",
       ...data
     });
-    await newTask.save();
-
-    return res.json(data);
   } catch (error) {
-    return res.json({
-      error,
-      message: error.message
+    catchError(res, error);
+  }
+}
+
+const updateTask = async(req, res) => {
+  const { description, list } = req.body;
+  const { name } = req.params;
+  try {
+    const task = await Task.findOne({ name });
+    task.description = description;
+    task.list = list;
+    await task.save();
+
+    return res.status(200).json({
+      message: 'Task updated successfully!',
+      task
     })
+  } catch (error) {
+    catchError(res, error);
+  }
+}
+
+const deleteTask = async(req, res) => {
+  const { name } = req.params;
+  try {
+   const task = await Task.deleteOne({ name });
+
+    if (!task.deletedCount) {
+      return res.status(400).json({
+        error: true,
+        message: "A task with that <<name>> not exists"
+      })
+    }
+    return res.status(200).json({
+      message: 'Deleted successfully',
+      task
+    })
+  } catch (error) {
+    catchError(res, error);
   }
 }
 
 module.exports = {
-  postTask
+  getTasks,
+  postTask,
+  updateTask,
+  deleteTask
 }
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
-const { postTask } = require('../controllers/tasks');
+const { postTask, getTasks, deleteTask, updateTask } = require('../controllers/tasks');
 const { postTaskValidator } = require('../middlewares/task-validator');
 
 const router = Router();
 
-router.get('/');
-router.post('/', postTaskValidator, postTask)
+router.get('/', getTasks);
+router.get('/:name', getTasks);
+router.post('/', postTaskValidator, postTask);
+router.post('/:name', updateTask);
+router.delete('/:name', deleteTask);
 
 module.exports = router;
